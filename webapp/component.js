0bbaec791d90aa5b346981de4d19dfc7c930e1e4
oFelpxz <felipe.ppepe0212@gmail.com>
2024-11-12 13:51:10 -0300

    Device Adaptation

--- webapp/component.js
+++ webapp/component.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
-  (UIComponent, JSONModel) => {
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel", "sap/ui/Device"],
+  (UIComponent, JSONModel, Device) => {
     "use strict";
 
     return UIComponent.extend("ui5.walkthrough.Component", {
@@ -8,17 +8,24 @@ sap.ui.define(
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
         manifest: "json",
       },
+
       init() {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
-        // set data models
+
+        // set data model
         const oData = {
           recipient: {
-            name: "UI5",
+            name: "World",
           },
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
+
+        // set device model
+        const oDeviceModel = new JSONModel(Device);
+        oDeviceModel.setDefaultBindingMode("OneWay");
+        this.setModel(oDeviceModel, "device");
 
         // create the views based on the url/hash
         this.getRouter().initialize();
